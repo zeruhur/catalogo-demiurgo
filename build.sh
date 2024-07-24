0a7fefb0ba71186ebc4c0fbeb273c86ebd4db694
zeruhur <51272598+zeruhur@users.noreply.github.com>
2024-07-24 13:51:00 +0200

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 asciidoctor-pdf -r asciidoctor-diagram main.adoc -a hyphens=it
-asciidoctor-epub3 -r asciidoctor-diagram main.adoc -a hyphens=it
+#asciidoctor-epub3 -r asciidoctor-diagram main.adoc -a hyphens=it
 asciidoctor -r asciidoctor-diagram main.adoc
-cp main.html /mnt/c/Users/utente/Documents/GitHub/gdrs/index.html
+cp main.html /mnt/c/Users/utente/Documents/GitHub/catalogo-demiurgo/index.html
 #cd /mnt/c/Users/utente/Documents/GitHub/gdrs/
 #git add index.html
 #git commit -m "update index.html"
